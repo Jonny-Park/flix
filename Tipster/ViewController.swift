c733c2058e9f091bbea7650b30f2b5bc74047e6f
Jonny-Park <park49j@mtholyoke.edu>
2021-11-18 23:10:13 -0500

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by 조은이꺼 on 11/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
