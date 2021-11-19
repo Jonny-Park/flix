9ab5c1d091555b423f97182d9ca248103b7a2052
Jonny-Park <park49j@mtholyoke.edu>
2021-11-19 00:23:21 -0500

    joeun week1 assignment

---
+++ Tipster/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  Tipster
+//
+//  Created by 조은이꺼 on 11/18/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
